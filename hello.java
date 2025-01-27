76bfff77a6b488e68e7b38fb01923128ee5da863
Vishal Bonde <165460908+Vishal9689@users.noreply.github.com>
2025-01-27 21:43:04 +0530

    Add files via upload
    this repository i am created to the help us java beginners and that is first program i am start with hello world

---
+++ hello.java
@@ -0,0 +1,7 @@
+class hello{
+    /*String name fist latter right on capital*/    
+    public static void main(String[]args){
+        /* System name fist latter right on capital*/    
+     System.out.println("hiii vishal");
+}
+}
