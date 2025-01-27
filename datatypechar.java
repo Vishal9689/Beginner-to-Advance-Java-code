ae7c2c192d8381df9a91927dbaa5409b3cbb854e
Vishal Bonde <165460908+Vishal9689@users.noreply.github.com>
2025-01-27 21:47:36 +0530

    Add files via upload
    this repository i am created to the help us java beginners😊

---
+++ datatypechar.java
@@ -0,0 +1,7 @@
+class datatypechar{
+    public static void main(String [] args){
+        /*character write in single quotation */
+         char sep =';';
+        System.out.println(sep);
+    }
+}
