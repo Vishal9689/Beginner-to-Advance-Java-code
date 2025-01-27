ad117918bc6ae2391dbc7f189f80483d37fd6a60
Vishal Bonde <165460908+Vishal9689@users.noreply.github.com>
2025-01-27 21:53:03 +0530

    Add files via upload
    2.This All File Fo Integer DataType

---
+++ datatypebyte.java
@@ -0,0 +1,18 @@
+/*Program1*/
+/*class datatypebytebyte{
+    public static void main( String[]args){
+        byte my_int;
+        my_int=23;
+        System.out.println(my_int);
+        
+    }
+}*/
+/*Program 2*/
+class datatypebyte{
+    public static void main(String[]args) {
+        byte my_int;
+        my_int=23;
+        my_int ++;
+        System.out.println(my_int);
+    } 
+}
---
+++ datatypeshort.java
@@ -0,0 +1,6 @@
+class datatypeshort{
+    public static void main(String [] args){
+        short my_int=520;
+      System.out.println(my_int);
+    }
+}
